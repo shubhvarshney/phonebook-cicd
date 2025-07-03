bc8d0d5c71f3672dd2c642ce2617b50ecca3a233
shubhvarshney <shubh.varshney@lhswildcats.org>
2025-07-03 15:14:17 -0500

    added default port

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ const errorHandler = (error, request, response, next) => {
 
 app.use(errorHandler)
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 4000
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Server running on port ${PORT}`)
