f2627e8f648bc8a83188035537717cef0c52317e
shubhvarshney <shubh.varshney@lhswildcats.org>
2025-07-03 14:49:32 -0500

    changed location of web Server

--- playwright.config.js
+++ playwright.config.js
@@ -25,14 +25,8 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  webServer: {
-    command: 'npm run start',
-    url: 'http://localhost:3000',
-    timeout: 120 * 1000,
-    reuseExistingServer: !process.env.CI,
-  },
   use: {
-    baseURL: 'http://localhost:3000/',
+    baseURL: 'http://localhost:3000',
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
 
@@ -69,10 +63,11 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run start',
+    url: 'http://localhost:3000',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120 * 1000, 
+  },  
 })
 
